2a52f18edafdd651a221c157f1b9fba90c4f7bb8
Dmitry-Ostashev <dmitry.ostashev@devexpress.com>
2021-11-30 15:09:59 +0300

    check if the file exists

--- tests/test.js
+++ tests/test.js
@@ -1,5 +1,19 @@
+import fs from 'fs';
+
 fixture`Screenshot tests`
-    .page`../index.html`;
+    .page`../index.html`
+    .before(() => {
+        if (fs.existsSync('./screenshots')) {
+            console.log('Folder contnet before: ');
+            console.log(fs.readdirSync('./screenshots'));
+        }
+    })
+    .after(() => {
+        if (fs.existsSync('./screenshots')) {
+            console.log('Folder contnet after: ');
+            console.log(fs.readdirSync('./screenshots'));
+        }
+    });
 
 test('Test', async t => {
     await t.expect(true).notOk();
